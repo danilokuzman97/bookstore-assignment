9752272357ac45de44969d5ae5d48726435c661e
Danilo <danilokuzman1@yahoo.com>
2026-01-04 18:47:14 +0100

    Prodavnica knjiga: Proširenje domenskog sloja i konvencija mapiranja  1. Deo zadatka

--- BookstoreApplication/BookstoreApplication/Models/AppDbContext.cs
+++ BookstoreApplication/BookstoreApplication/Models/AppDbContext.cs
@@ -11,5 +11,26 @@ namespace BookstoreApplication.Models
         public DbSet<Author> Authors { get; set; }
         public DbSet<Book> Books { get; set; }
         public DbSet<Publisher> Publishers { get; set; }
+        public DbSet<Award> Awards { get; set; }
+        public DbSet<AuthorAward> AuthorAwards { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<AuthorAward>(authorAwardEntity => { 
+              authorAwardEntity.HasKey(authorAward => new { authorAward.AuthorId, authorAward.AwardId }); // Primarni ključ je kombinacija AuthorId + AwardId
+
+                // Veza ka Author
+                authorAwardEntity.HasOne(authorAward => authorAward.Author)
+                               .WithMany(author => author.AuthorAwards)
+                               .HasForeignKey(authorAward => authorAward.AuthorId);
+
+                // Veza ka Award
+              authorAwardEntity.HasOne(authorAward => authorAward.Award)
+                               .WithMany(award => award.AuthorAwards)
+                               .HasForeignKey(authorAward => authorAward.AwardId);
+            });
+        }
     }
 }
---
+++ BookstoreApplication/BookstoreApplication/Models/Award.cs
@@ -0,0 +1,13 @@
+﻿namespace BookstoreApplication.Models
+{
+    public class Award
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Established { get; set; }
+        public ICollection<AuthorAward> AuthorAwards { get; set; }
+
+
+    }
+}
